b503551741ea35b850776e1124f876ffcb652444
Chabulsqu <mateofainalonso@gmail.com>
2022-06-29 20:52:01 -0300

    Finished basic layout

--- resources/main.js
+++
@@ -1,6 +0,0 @@
-let checkbox = document.querySelector('input');
-let root = document.querySelector(':root');
-function toggle () {
-   let computedStyle = getComputedStyle(root);
-   computedStyle.getPropertyValue('--text-color') === 'hsl(0, 0%, 9%)' ? checkbox.style.setProperty('--text-color', 'hsl(0, 0%, 99%)') : checkbox.style.setProperty('--text-color', 'hsl(0, 0%, 9%)');
-}
