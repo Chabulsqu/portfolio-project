9c858e8de7317db672738b4e82018bf3ffbfa890
Chabulsqu <mateofainalonso@gmail.com>
2022-07-10 13:55:54 -0300

    Finished Responsive Layout for bigger screen sizes

--- resources/main.js
+++ resources/main.js
@@ -1,22 +1,60 @@
-let head = document.head;
+let icon = document.getElementById('mode-toggle');
 let link;
 let dropdowns = document.getElementsByClassName('about');
+let firstDrop = document.getElementById('first-drop');
+let secondDrop = document.getElementById('second-drop');
+let imageContainers = document.getElementsByClassName('image-container');
+let w = document.documentElement.clientWidth;
+
+if (document.querySelector('body').className == 'dark') {
+    firstDrop.style.filter = 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(214deg) brightness(116%) contrast(108%)';
+    secondDrop.style.filter = 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(214deg) brightness(116%) contrast(108%)';
+    icon.alt = 'Toggle Light mode';
+    icon.src = './resources/images/light-mode.svg';
+    icon.style.filter = 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(214deg) brightness(116%) contrast(108%)';
+    for (let box of imageContainers) {
+      box.style.backgroundColor = 'var(--secondary-background-color)';
+      box.style.filter = 'none';
+    }
+} else {
+    icon.alt = '.Toggle Dark Mode';
+    icon.src = './resources/images/dark-mode.svg';
+    icon.style.filter = 'none';
+    for (let box of imageContainers) {
+      box.style.backgroundColor = '';
+      box.style.filter = 'drop-shadow(9px 9px 3px var(--secondary-background-color))';
+    }
+}
 
 const toggleMode = () => {
     if (document.querySelector('body').className == 'dark') {
-        document.querySelector('body').classList.replace('dark', 'light');
+        document.body.classList.replace('dark', 'light');
+        firstDrop.style.filter = '';
+        secondDrop.style.filter = '';
         document.head.removeChild(link);
-        dropdowns[0].src = './resources/images/dropdown_arrow_light.svg';
-        dropdowns[1].src = './resources/images/dropdown_arrow_light.svg';
-    } else { 
-        document.querySelector('body').classList.replace('light', 'dark');
+        icon.src = './resources/images/dark-mode.svg';
+        icon.alt = 'Toggle Dark mode';
+        icon.style.filter = 'none';
+        for (let box of imageContainers) {
+         box.style.backgroundColor = '';
+         box.style.filter = 'drop-shadow(9px 9px 3px var(--secondary-background-color))';
+       }
+    } else {
+        document.body.classList.replace('light', 'dark');
         link = document.createElement('link');
         document.head.appendChild(link);
-        link.href = './zbeforepseudoclass.css';
+        link.href = './resources/zbeforepseudoclass.css';
         link.type = 'text/css';
         link.rel = 'stylesheet';
-        dropdowns[0].src = './resources/images/dropdown_arrow_dark.svg';
-        dropdowns[1].src = './resources/images/dropdown_arrow_dark.svg';
+        icon.src = './resources/images/light-mode.svg';
+        icon.alt = 'Toggle Light mode';
+        icon.style.filter = 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(214deg) brightness(116%) contrast(108%)';
+        firstDrop.style.filter = 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(214deg) brightness(116%) contrast(108%)';
+        secondDrop.style.filter = 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(214deg) brightness(116%) contrast(108%)';
+        for (let box of imageContainers) {
+         box.style.backgroundColor = 'var(--secondary-background-color)';
+         box.style.filter = 'none';
+       }
     }
 };
 
